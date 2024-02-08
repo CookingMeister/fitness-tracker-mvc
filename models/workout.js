a77d72dcc555e23151486477bedb5d5c33529701
Samantha Allen <samanthaamiraallen@gmail.com>
2024-02-08 08:57:12 -0400

    changed post.js to workout.js and created model

---
+++ models/workout.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Workout extends Model {}
+
+Workout.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        day: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlpha: true,
+            },
+        },
+        exercise: {
+            type: DataTypes.STRING,
+            validate: {
+                isAlpha: true,
+            },
+        },
+        reps: {
+            type: DataTypes.INTEGER,
+            validate: {
+                isNumeric: true,
+            },
+        },
+        sets: {
+            type: DataTypes.INTEGER,
+            validate: {
+                isNumeric: true,
+            },
+        },
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'workout',
+    }
+);
+
+module.exports = Workout;
