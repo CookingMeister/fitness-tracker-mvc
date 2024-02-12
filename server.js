75c446f0e6c50bdae0b3a55da74a5c37b22b76ca
MaikaSaba <saba.maika@gmail.com>
2024-02-12 17:02:18 -0400

    buttons added for activities, footer WIP

--- server.js
+++ server.js
@@ -1,42 +1,45 @@
 // server.js
-const express = require('express');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-const { sequelize } = require('./models');
-
+const { sequelize } = require("./models");
+console.log("Connection - ", sequelize);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Set EJS as the view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Session setup
 const sessionStore = new SequelizeStore({ db: sequelize });
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store: sessionStore,
-}));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  })
+);
 
 // Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-const apiRoutes = require('./routes/api/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require("./routes/api/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 // Sync Sequelize models and start the server
 sequelize.sync({ force: false }).then(() => {
