200ade9e0e13c4d0684de31da38ce306eba6b6c1
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-02-13 21:05:18 -0400

    add reload to user.ejs, login route expanded, user model password parameters expanded

--- models/user.js
+++ models/user.js
@@ -23,7 +23,7 @@ User.init(
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [8, 20]
+                len: [8, 70]
             },
         },
         height: {
--- routes/api/login-route.js
+++ routes/api/login-route.js
@@ -10,27 +10,30 @@ router.post('/', (req, res) => {
     where: {
       username: req.body.username,
     },
-  }).then((dbUserData) => {
-    if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that username!' });
-      return;
-    }
-    // const validPassword = dbUserData.checkPassword(req.body.password);
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that username!' });
+        return;
+      }
+      // const validPassword = dbUserData.checkPassword(req.body.password);
 
-    // if (!validPassword) {
-    //   res.status(400).json({ message: 'Incorrect password!' });
-    //   return;
-    // }
+      // if (!validPassword) {
+      //   res.status(400).json({ message: 'Incorrect password!' });
+      //   return;
+      // }
 
-    req.session.save(() => {
-      req.session.id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
+      req.session.save(() => {
+        req.session.id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
 
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'An error occurred', error: err });
     });
-  });
 });
-
 
 module.exports = router;
