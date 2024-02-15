ae11a309d69cf228cebde87574f498a6b682b4ee
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-02-15 16:51:15 -0400

    add logout route, ejs, js, working

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+document.querySelector('.logout-btn').addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('Logout clicked!');
+    const username = document.querySelector('input[name= "username"]').value;
+
+    const user = { username };
+    const postRequest = axios.post('/api/logout', user);
+    postRequest.
+        then(function (response) {
+          console.log('Response:', response.data);
+          window.location.replace('/');
+        })
+        .catch(function (error) {
+          console.error('Error:', error.message);
+        });
+});
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,10 +3,12 @@ const dashboardRoutes = require('./dashboard-route');
 const userRoutes = require('./user-profile-route');
 const registerRoutes = require('./register-route');
 const loginRoutes = require('./login-route');
+const logoutRoutes = require('./logout-route');
 
 router.use('/register', registerRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/user', userRoutes);
 router.use('/login', loginRoutes);
+router.use('/logout', logoutRoutes);
 
 module.exports = router;
